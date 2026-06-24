f3b3c7b15972b62a4d1e42d6225c986aad7d37b8
Talha Rehman <talharehman2567@gmail.com>
2026-06-25 00:51:28 +0500

    third section of JSE

--- phase1/JSE1/MODULE1/SECTION3/index.js
+++ phase1/JSE1/MODULE1/SECTION3/index.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+console.log("Talha Rehman");
+console.log("06-06-2005");
+console.log(06-06-2005);
+console.log("talha","rehman","06-06-2005");
+console.log('talha')
+console.log('rehman');
+console.log(2005);
+console.log("talha " + "rehman " + "06-06-2005");
+
+
+
---
+++ phase1/main.js
@@ -0,0 +1,13 @@
+let names = [];
+let isOver = false;
+while (!isOver) {
+    let name = Prompt("Enter another name or press cancel.")
+    if (name != null) {
+     names.push(name);
+    } else {
+     isOver = true;
+    }
+}
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
