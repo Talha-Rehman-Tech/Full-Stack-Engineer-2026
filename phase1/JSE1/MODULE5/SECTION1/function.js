e35230bbf750109e90281c18a2fbb0e069d2a18f
Talha Rehman <talharehman2567@gmail.com>
2026-06-28 11:10:28 +0500

    day five learn function and many more

---
+++ phase1/JSE1/MODULE5/SECTION1/function.js
@@ -0,0 +1,32 @@
+
+let names = ["Alice", "Bob", "Eve", "John"];
+let name = getElement(names, 2);
+console.log(name); 
+
+function getMeanTemp(temperatures) {
+     if (!(temperatures instanceof Array)) {
+         return NaN;
+     }
+     let sum = 0;
+     for (let i = 0; i < temperatures.length; i++) {
+         sum += temperatures[i];
+     }
+     return sum / temperatures.length;
+}
+console.log(getMeanTemp(10));       // -> NaN
+console.log(getMeanTemp([10, 30])); // -> 20
+
+function factorial (n) {
+     let result = 1;
+     while (n > 1) {
+      result *= n;
+      n--;
+     }
+     return result;
+}
+console.log(factorial(6)); // -> 720
+
+function factorial (n) {
+     return n > 1 ? n * factorial(n - 1) : 1;
+}
+console.log(factorial(6));
